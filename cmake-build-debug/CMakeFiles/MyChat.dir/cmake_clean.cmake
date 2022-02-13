b8f55d99b183080c59dbf4265c4d644c44c77579
Hugo Fernández Solís <hugofernandezsolis@gmail.com>
2022-02-13 17:05:45 +0000

    Repo init

---
+++ cmake-build-debug/CMakeFiles/MyChat.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyChat.dir/main.cpp.o"
+  "MyChat"
+  "MyChat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyChat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
